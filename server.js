8149608a7591191bcb6dbae3c7b5a860ce701232
Tony Wu <wuyuehao@gmail.com>
2015-08-24 09:42:14 -0700

    add a post service

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var service = require('./service')
 var app = express();
 
 app.get('/data', service.findAll);
-
+app.post('/data', service.create);
 
 app.listen(3000);
 console.log('Listening on port 3000...');
--- service.js
+++ service.js
@@ -1,8 +1,13 @@
+var data = "{x:["7:00", "8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00", "17:00","18:00"], tier1:[99,98,97,99,98,97,99,98,97,99,98,97], tier2:[96,95,94,96,95,94,96,95,94,96,95,94], concurrency:[0,0,0,1000,0,0,1000,0,0,1000,0,1000], cluster:[0,0,750,0,0,750,0,0,750,0,0,750]}";
+
 exports.findAll = function(req, res) {
     	res.header("Content-Type", "application/json");
-	res.send({x:["7:00", "8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00", "17:00","18:00"], tier1:[99,98,97,99,98,97,99,98,97,99,98,97], tier2:[96,95,94,96,95,94,96,95,94,96,95,94], concurrency:[0,0,0,1000,0,0,1000,0,0,1000,0,1000], cluster:[0,0,750,0,0,750,0,0,750,0,0,750]});
+	res.send(data);
     };
 
-exports.findById = function(req, res) {
-        res.send({id:req.params.id, name: "The Name", description: "description"});
+
+
+exports.create = function(req, res) {
+	data = req.body;
+        res.send(data);
 	};
